a3899c5b81d07491c7f42b0ee38c6261d2cf7e9e
Daniel G <dani75esp@gmail.com>
2022-12-14 19:10:49 +0100

    First commit

---
+++ src/main/java/com/svalero/ERM/EmergencyReportingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.ERM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyReportingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyReportingSystemApplication.class, args);
+	}
+
+}
